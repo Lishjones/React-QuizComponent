d304098f701ac32631cc6adcdeab530b8366aa49
Lishjones <49446723+Lishjones@users.noreply.github.com>
2020-06-01 12:53:34 +0100

    Module5 questionbutton fix click

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 class QuizQuestionButton extends Component {
 
-handleClick(button_text) {
-this.props.clickHandler();
+handleClick() {
+this.props.clickHandler(this.props.button_text);
 }
 
 render() {
